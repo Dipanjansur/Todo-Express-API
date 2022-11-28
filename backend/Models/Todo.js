e9830ed99e001f9858258067f20500988d4a1945
dipanjansur <dip20.sur@gmail.com>
2022-11-28 21:24:13 +0530

    timestamp added and minor fix

--- backend/Models/Todo.js
+++ backend/Models/Todo.js
@@ -3,7 +3,7 @@ const todo = mongoose.Schema({
 	email: String,
 	tittle: { type: String, require: true },
 	todo: { type: [String] }
-});
+}, { timestamp: true });
 module.exports = mongoose.model('todo', todo);
 
 
